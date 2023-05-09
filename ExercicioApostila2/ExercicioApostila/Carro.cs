a5cc257f42de0480b484d1a6f13584ace03b0edc
Wesley Lima <wesleyaz0909@gmail.com>
2023-05-08 21:59:50 -0300

    Add files via upload

---
+++ ExercicioApostila2/ExercicioApostila/Carro.cs
@@ -0,0 +1,19 @@
+﻿public class Carro
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public string Cor { get; set; }
+    public int VelocidadeMaxima { get; set; }
+    public int Potencia { get; set; }
+
+    public string Acelerar()
+    {
+        return "O carro está em movimento";
+    }
+
+    public string Freiar()
+    {
+        return "O carro está parado";
+    }
+}
---
+++ ExercicioApostila2/ExercicioApostila/Execucao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioApostila
+{
+    internal class Execucao
+    {
+        public static void Main(string[] args)
+        {
+            Carro carro1 = new Carro();
+            carro1.Marca = "Chevrolet";
+            carro1.Modelo = "Camaro";
+            carro1.Ano = 2022;
+            carro1.Cor = "Amerelo";
+            carro1.VelocidadeMaxima = 290;
+            carro1.Potencia = 455;
+
+            Console.WriteLine("Carro 1:");
+            Console.WriteLine("Marca: " + carro1.Marca);
+            Console.WriteLine("Modelo: " + carro1.Modelo);
+            Console.WriteLine("Ano: " + carro1.Ano);
+            Console.WriteLine("Cor: " + carro1.Cor);
+            Console.WriteLine("Velocidade Máxima: " + carro1.VelocidadeMaxima);
+            Console.WriteLine("Potência: " + carro1.Potencia);
+
+            string run = carro1.Acelerar();
+            Console.WriteLine(run);
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            run = carro1.Freiar();
+            Console.WriteLine(run);
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine(".");
+            Console.WriteLine();
+        }
+    }
+}
